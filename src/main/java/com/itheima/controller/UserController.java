8cc1420229ae269798eab4155749381a03615c90
xuebusi <xuebusi@qq.com>
2024-02-28 20:57:54 +0800

    注册接口参数校验&全局异常处理器

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -4,10 +4,14 @@ import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Pattern;
+
+@Validated
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -15,7 +19,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,
+                           @Pattern(regexp = "^\\S{5,16}$") String password) {
         // 查询用户名是否存在
         User u = userService.findByUsername(username);
         if (u == null) {
